c52452f95e2c5ac4454852d8cf97c21b7c394c30
Nattawut <ackenaten@gmail.com>
2016-06-16 22:08:48 +0700

    Initialize project

---
+++ www/js/controllers.js
@@ -0,0 +1,63 @@
+angular.module('starter.controllers', [ 'ionic-material', 'ionMdInput'])
+
+.controller('DashCtrl', function($scope, $ionicModal) {
+  $scope.list = [];
+  $scope.totalCost = 0;
+  $ionicModal.fromTemplateUrl('templates/modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function() {
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+
+  $scope.addList = function() {
+    var num = $scope.list.length;
+
+    $scope.list.push({title: 'title'+num, cost: 100});
+    $scope.totalCost +=  (100 - (20/100) *100);
+    console.log($scope.list);
+  }
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
